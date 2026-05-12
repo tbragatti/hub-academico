7876e340ab67942e60870d39475739fdac3c2e58
Tbragatti <thallesbragatti@gmail.com>
2026-05-12 11:15:06 -0300

    initial commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+
+
+
+
+export function HomePage() {
+  return (
+    <h1>Home</h1>
+  )
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import {
+  createBrowserRouter,
+} from 'react-router-dom'
+
+import { HomePage } from '../pages/home'
+import { LoginPage } from '../pages/login'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+])
